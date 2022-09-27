1fcf93bc84580ae3f1277097475b38e441ce8486
frontpro <aly.arbab@gmail.com>
2022-09-27 15:02:48 +0200

    switch loads

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const ham = document.querySelector(".ham");
 const navlinks= document.querySelector(".nav-links");
+const switches = document.querySelector(".slider");
+const mo = document.querySelectorAll(".months");
+const ye = document.querySelectorAll(".years");
+
 
 
 ham.addEventListener("click",()=>{
     navlinks.classList.toggle("open");
 });
+window.addEventListener('DOMContentLoaded',function(){
+    mo.forEach(element => {
+        element.classList.add("show");
+        })
+    });
+    
+switches.addEventListener('click',function(){
+    if(!mo.classList.contains("show")){
+        mo.classList.add("show");
+        ye.classList.remove("show")
+    }
+    else{
+        mo.classList.remove("show");
+        ye.classList.add("show")
+    }
+})
