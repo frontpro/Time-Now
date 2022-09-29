6e8918f908e30491eba3450109485499ccfdf4f9
frontpro <aly.arbab@gmail.com>
2022-09-29 06:50:12 +0200

    final

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const navlinks= document.querySelector(".nav-links");
 const switches = document.querySelector(".slider");
 const mo = document.querySelectorAll(".months");
 const ye = document.querySelectorAll(".years");
-
+const monthBill = document.querySelector(".month-bill");
+const yearBill = document.querySelector(".year-bill");
 
 
 ham.addEventListener("click",()=>{
@@ -17,16 +18,22 @@ window.addEventListener('DOMContentLoaded',function(){
     
 switches.addEventListener('click',function(){
     mo.forEach(function(i){
-        ye.forEach(function(j){
-            if(!i.classList.contains("show")){
-                i.classList.add("show");
-                j.classList.remove("show")
-            }
-            else{
-                i.classList.remove("show");
-                j.classList.add("show")
-            }
-        })
+        if(!i.classList.contains("show")){
+            i.classList.add("show");
+        }
+        else{
+            i.classList.remove("show");
+        } 
+    });
+    ye.forEach(function(j){
+        if(!j.classList.contains("show")){
+            j.classList.add("show")
+        }
+        else{
+            j.classList.remove("show");
+        } 
     })
     
 })
+
+
