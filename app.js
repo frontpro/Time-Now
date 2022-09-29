e81e669efd49e9fd2d6df2b08d34872c194bbad8
frontpro <aly.arbab@gmail.com>
2022-09-29 10:03:50 +0200

    final product

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mo = document.querySelectorAll(".months");
 const ye = document.querySelectorAll(".years");
 const monthBill = document.querySelector(".month-bill");
 const yearBill = document.querySelector(".year-bill");
+const checkBox = document.querySelector(".checkbox");
 
 
 ham.addEventListener("click",()=>{
@@ -14,23 +15,30 @@ window.addEventListener('DOMContentLoaded',function(){
     mo.forEach(element => {
         element.classList.add("show");
         })
+        monthBill.style.fontWeight = "700";
     });
     
 switches.addEventListener('click',function(){
     mo.forEach(function(i){
         if(!i.classList.contains("show")){
             i.classList.add("show");
+        monthBill.style.fontWeight = "700";
         }
         else{
             i.classList.remove("show");
+        monthBill.style.fontWeight = "500";
+
         } 
     });
     ye.forEach(function(j){
         if(!j.classList.contains("show")){
             j.classList.add("show")
+            yearBill.style.fontWeight = "700";
         }
         else{
             j.classList.remove("show");
+            yearBill.style.fontWeight = "500";
+
         } 
     })
     
